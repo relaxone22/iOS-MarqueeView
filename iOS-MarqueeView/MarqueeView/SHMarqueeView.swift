17970b926f2dae8d933742e81b2c9c22beca1c60
A70093 <eA70093@tdcc.com.tw>
2022-05-04 11:00:01 +0800

    Initial version

---
+++ iOS-MarqueeView/MarqueeView/SHMarqueeView.swift
@@ -0,0 +1,20 @@
+//
+//  SHMarqueeView.swift
+//  iOS-MarqueeView
+//
+//  Created by TDCC_IFD on 2022/5/4.
+//
+
+import UIKit
+
+class SHMarqueeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
