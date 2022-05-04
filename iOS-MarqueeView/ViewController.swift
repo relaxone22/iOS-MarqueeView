d729325e67371e7037f54550f02df75c319e161f
A70093 <eA70093@tdcc.com.tw>
2022-05-04 10:48:04 +0800

    Initial Commit

---
+++ iOS-MarqueeView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-MarqueeView
+//
+//  Created by TDCC_IFD on 2022/5/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
